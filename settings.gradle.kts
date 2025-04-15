2abda44c0e901d027d12741d28e7ac1304e35a8d
rmorenko <v-roman.morenko@hazelcast.com>
2025-04-15 12:22:00 +0200

    Add pipeline for publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "auto-mapper"
 include(":auto-mapper-processor")
 include(":auto-mapper-workaround")
